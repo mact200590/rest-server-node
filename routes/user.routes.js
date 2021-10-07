23b44e370affc406580d93c6d79ec2d8b72c2198
Manuel Alejandro <mact200590@gmail.com>
2021-10-07 12:41:57 -0400

    add routes

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,12 +1,58 @@
-const { Router } = require("express");
-const { getUsers, putUser, postUser, patchUser, deleteUser } = require("../controls/user.controls");
+const { Router, response } = require("express");
+const { check } = require("express-validator");
+const {
+  validateRole,
+  emailExist,
+  existUserId,
+} = require("../helpers/db_validator");
+const {
+  getUsers,
+  putUser,
+  postUser,
+  patchUser,
+  deleteUser,
+} = require("../controls/user.controls");
+const { validateFields } = require("../middleware/validate_field");
 
 const router = Router();
 
-router.get("/",getUsers);
-router.put("/:id", putUser);
-router.post("/",postUser );
-router.patch("/", patchUser)
-router.delete("/", deleteUser);
+router.get("/", getUsers);
+router.put(
+  "/:id",
+  [
+    check("id", "The id provided is not valid").isMongoId(),
+    check("id").custom(existUserId),
+    check("role").custom(validateRole),
+    validateFields,
+  ],
+  putUser
+);
+router.post(
+  "/",
+  [
+    check("name", "The name is required").not().isEmpty(),
+    check("email", "The email is no correct").isEmail(),
+    check("email", "The email is no correct").custom((email) =>
+      emailExist(email)
+    ),
+    check(
+      "password",
+      "The password should contains more than 6 letters"
+    ).isLength({ min: 6 }),
+    check("role").custom(validateRole),
+    validateFields,
+  ],
+  postUser
+);
+router.patch("/", patchUser);
+router.delete(
+  "/:id",
+  [
+    check("id", "The id provided is not valid").isMongoId(),
+    check("id").custom(existUserId),
+    validateFields
+  ],
+  deleteUser
+);
 
 module.exports = router;
